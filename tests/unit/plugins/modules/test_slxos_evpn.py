953936d970aef7ee0c701106ac8288b67d75514b
sfulmer <sfulmer@redhat.com>
2026-05-16 11:07:47 -0400

    Add future imports to test files for sanity

--- tests/unit/plugins/modules/test_slxos_evpn.py
+++ tests/unit/plugins/modules/test_slxos_evpn.py
@@ -1,3 +1,6 @@
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
 """Unit tests for slxos_evpn module."""
 
 from unittest.mock import MagicMock
--- tests/unit/plugins/modules/test_voss_interface.py
+++ tests/unit/plugins/modules/test_voss_interface.py
@@ -1,3 +1,6 @@
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
 """Unit tests for voss_interface module."""
 
 from unittest.mock import MagicMock
